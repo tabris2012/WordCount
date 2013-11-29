5a2fdf4aa0f4d78381c34c3b250b9d0353e6a994
tazro inutano ohta <inutano@gmail.com>
2013-11-29 17:13:27 +0900

    metadata parser required parallel and nokogiri

---
+++ parse_design_desc.rb
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+require "nokogiri"
+require "parallel"
+require "json"
+
+def get_all_exp(target_dir)
+  Dir.glob(target_dir+"/*RA*/*.experiment.xml")
+end
+
+def parse_design(xml_path)
+  nkgr = Nokogiri::XML(open(xml_path))
+  exp_array = nkgr.css("EXPERIMENT")
+  exp_array.map do |exp|
+    id = exp.attr("accession").to_s
+    design = exp.css("DESIGN_DESCRIPTION").inner_text.gsub("\n","")
+    [id, design]
+  end
+end
+
+if __FILE__ == $0
+  list_xml_path = get_all_exp("./NCBI_SRA_Metadata_Full_20130801")
+  id_desc = Parallel.map(list_xml_path) do |xml_path|
+    parse_design(xml_path)
+  end
+  open("./result.json"){|f| JSON.dump(id_desc, f)}
+end
