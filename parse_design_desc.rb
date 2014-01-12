d54749655931726216c6c93bbf7c6a51d7f91738
tazro inutano ohta <inutano@gmail.com>
2014-01-12 13:37:48 +0900

    multispecing into single, threading limit

--- parse_design_desc.rb
+++ parse_design_desc.rb
@@ -13,14 +13,15 @@ def parse_design(xml_path)
   exp_array = nkgr.css("EXPERIMENT")
   exp_array.map do |exp|
     id = exp.attr("accession").to_s
-    design = exp.css("DESIGN_DESCRIPTION").inner_text.gsub("\n","")
+    design_text = exp.css("DESIGN_DESCRIPTION").inner_text
+    design = design_text.gsub("\n","").gsub(/\s+/,"\s")
     [id, design]
   end
 end
 
 if __FILE__ == $0
   list_xml_path = get_all_exp("./NCBI_SRA_Metadata_Full_20130801")
-  id_desc = Parallel.map(list_xml_path) do |xml_path|
+  id_desc = Parallel.map(list_xml_path, :in_threads => 12) do |xml_path|
     parse_design(xml_path)
   end
   open("./result.json","w"){|f| JSON.dump(id_desc.flatten(1), f) }
